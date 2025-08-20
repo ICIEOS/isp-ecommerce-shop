7413273723787ac676436620c28c68a7cda3d500
saranga423 <162134044+saranga423@users.noreply.github.com>
2025-08-20 09:51:03 +0530

    Countdown

--- components/elements/CountDown.js
+++ components/elements/CountDown.js
@@ -7,53 +7,54 @@ const msInAHour = 60 * msInMinute
 const msInADay = 24 * msInAHour
 
 const getPartsofTimeDuration = (duration) => {
-    const days = Math.floor(duration / msInADay)
-    const hours = Math.floor((duration % msInADay) / msInAHour)
-    const minutes = Math.floor((duration % msInAHour) / msInMinute)
-    const seconds = Math.floor((duration % msInMinute) / msInSecond)
+  const days = Math.floor(duration / msInADay)
+  const hours = Math.floor((duration % msInADay) / msInAHour)
+  const minutes = Math.floor((duration % msInAHour) / msInMinute)
+  const seconds = Math.floor((duration % msInMinute) / msInSecond)
 
-    return { days, hours, minutes, seconds }
+  return { days, hours, minutes, seconds }
 }
 
-const Countdown = (endDateTime) => {
-    const [time, setTime] = useState(new Date().toLocaleTimeString())
+const Countdown = ({ endDateTime }) => {
+  const [timeParts, setTimeParts] = useState(null)
 
-    useEffect(() => {
-        const timeout = setTimeout(() => {
-            const date = new Date()
-            setTime(date.toLocaleTimeString())
-        }, 1000)
-        return () => {
-            clearTimeout(timeout)
-        }
-    }, [time])
+  useEffect(() => {
+    const updateCountdown = () => {
+      const now = Date.now()
+      const future = new Date(endDateTime)
+      const timeDif = future.getTime() - now
+      setTimeParts(getPartsofTimeDuration(Math.max(0, timeDif)))
+    }
 
-    const now = Date.now() // Number of milliseconds from begining of time
+    updateCountdown() // run once immediately
+    const timer = setInterval(updateCountdown, 1000)
 
-    const future = new Date(endDateTime.endDateTime) // The day we leave for Japan
+    return () => clearInterval(timer)
+  }, [endDateTime])
 
-    const timeDif = future.getTime() - now
-    const timeParts = getPartsofTimeDuration(timeDif)
+  // 🚨 Prevent hydration mismatch: don't render until mounted
+  if (!timeParts) return null
 
-    // const countDownTime = `${timeParts.days} Days ${timeParts.hours} Hours and ${timeParts.minutes} minutes and ${timeParts.seconds} seconds`;
-    return (
-        <>
-            <span className="cdown days"> <span className="time-count">{timeParts.days}</span>
-                <p>Days</p>
-            </span>
-            <span className="cdown hour"> <span className="time-count">{timeParts.hours}</span>
-                <p>Hour</p>
-            </span>
-            <span className="cdown minutes"> <span className="time-count">{timeParts.minutes}</span>
-                <p>Minute</p>
-            </span>
-            <span className="cdown second"> <span className="time-count">{timeParts.seconds}</span>
-                <p>Second</p>
-            </span>
-
-
-        </>
-    )
+  return (
+    <>
+      <span className="cdown days">
+        <span className="time-count">{timeParts.days}</span>
+        <p>Days</p>
+      </span>
+      <span className="cdown hour">
+        <span className="time-count">{timeParts.hours}</span>
+        <p>Hour</p>
+      </span>
+      <span className="cdown minutes">
+        <span className="time-count">{timeParts.minutes}</span>
+        <p>Minute</p>
+      </span>
+      <span className="cdown second">
+        <span className="time-count">{timeParts.seconds}</span>
+        <p>Second</p>
+      </span>
+    </>
+  )
 }
 
 export default Countdown
--- components/filter/PriceRangeSlider.js
+++ components/filter/PriceRangeSlider.js
@@ -1,45 +1,65 @@
 'use client'
 import { useEffect, useState } from "react"
-import InputRange from "react-input-range"
+import { Range } from "react-range"
 import { useDispatch, useSelector } from "react-redux"
 import { addprice } from "../../features/filterSlice"
 
 const PriceRangeSlider = () => {
     const { shopList } = useSelector((state) => state.filter)
 
-    const [price, setprice] = useState({
-        min: shopList.price.min,
-        max: shopList.price.max,
-    })
+    const [price, setPrice] = useState([shopList.price.min, shopList.price.max])
 
     const dispatch = useDispatch()
 
     // price handler
-    const handleOnChange = ({ min, max }) => {
-        dispatch(addprice({ min, max }))
+    const handleOnChange = (values) => {
+        setPrice(values)
+        dispatch(addprice({ min: values[0], max: values[1] }))
     }
 
     useEffect(() => {
-        setprice({
-            min: shopList.price.min,
-            max: shopList.price.max,
-        })
-    }, [setprice, shopList])
+        setPrice([shopList.price.min, shopList.price.max])
+    }, [shopList])
 
     return (
         <div className="range-slider-one">
-            <InputRange
-                formatLabel={(value) => ``}
-                minValue={0}
-                maxValue={100}
-                value={{ min: price.min, max: price.max }}
-                onChange={(value) => handleOnChange(value)}
+            <Range
+                step={1}
+                min={0}
+                max={100}
+                values={price}
+                onChange={handleOnChange}
+                renderTrack={({ props, children }) => (
+                    <div
+                        {...props}
+                        style={{
+                            ...props.style,
+                            height: "6px",
+                            width: "100%",
+                            backgroundColor: "#ccc",
+                            borderRadius: "4px",
+                        }}
+                    >
+                        {children}
+                    </div>
+                )}
+                renderThumb={({ props }) => (
+                    <div
+                        {...props}
+                        style={{
+                            ...props.style,
+                            height: "20px",
+                            width: "20px",
+                            borderRadius: "50%",
+                            backgroundColor: "#007bff",
+                        }}
+                    />
+                )}
             />
 
             <div className="input-outer">
                 <div className="amount-outer">
-                    <span className="area-amount">{price.max}$ </span>
-
+                    <span className="area-amount">{price[0]}$ – {price[1]}$</span>
                 </div>
             </div>
         </div>
